6456a539ced16931fb2d74652d6a7f87d9b4a982
Zhangyi <1352869124@qq.com>
2022-04-11 19:52:34 +0800

    second commit

--- src/main/java/com/zhangyi/git/HelloGit.java
+++ src/main/java/com/zhangyi/git/HelloGit.java
@@ -6,6 +6,6 @@ package com.zhangyi.git;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git 2");
     }
 }
