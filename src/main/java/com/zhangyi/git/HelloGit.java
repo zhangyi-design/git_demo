cc2c9c5845050b8c2e5d4304d2182e84de98c714
Zhangyi <1352869124@qq.com>
2022-04-11 20:45:01 +0800

    master commit2

--- src/main/java/com/zhangyi/git/HelloGit.java
+++ src/main/java/com/zhangyi/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("Hello Git 4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }
