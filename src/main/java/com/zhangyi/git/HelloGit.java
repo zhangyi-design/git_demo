f62cb7cc996e2d6431d55c61492b7a3cbb75aaa9
Zhangyi <1352869124@qq.com>
2022-04-11 20:15:33 +0800

    hot-fix commit1

--- src/main/java/com/zhangyi/git/HelloGit.java
+++ src/main/java/com/zhangyi/git/HelloGit.java
@@ -6,6 +6,6 @@ package com.zhangyi.git;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git 3");
+        System.out.println("Hello Git 4");
     }
 }
