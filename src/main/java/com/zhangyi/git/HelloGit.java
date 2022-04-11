812ac682f1dbf114a6eab7bcf7eb2ea987a9d165
Zhangyi <1352869124@qq.com>
2022-04-11 18:12:23 +0800

    first commit

---
+++ src/main/java/com/zhangyi/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.zhangyi.git;
+
+/**
+ * @author zhangyi
+ * @create 2022-04-11 18:08
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
